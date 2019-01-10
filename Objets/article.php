0cb6d5a99ba2b7ef5120f22e36249a4bebab52e4
Valentin Beaury <beaury.valentin@gmail.com>
2019-01-10 11:43:52 +0100

    mise en page terminée

---
+++ Objets/article.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Valentin Beaury
+ * Date: 10/01/2019
+ * Time: 11:43
+ */
+
+class article
+{
+
+}
--- index.php
+++ index.php
@@ -26,9 +26,44 @@
         </div>
 
     </nav>
+
+
     <section id="section">
         <article class="article">
+            <div class="bandeau_article">
+                <span>#Posté le jeudi 10 janvier 2019 à 10:40</span>
+            </div>
+            <h2>Titre de l'article</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A asperiores autem, blanditiis cum deleniti
+                deserunt ea eum exercitationem incidunt mollitia nisi nulla, perspiciatis quaerat quod repellat saepe
+                similique temporibus ut.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at delectus dolores dolorum eos
+                impedit in magnam maiores minima qui! At earum nobis possimus sapiente tempore! Dolores laudantium
+                nesciunt recusandae?
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa deleniti eaque enim et facilis fugit
+                harum hic id incidunt, maiores modi molestiae quas quibusdam quidem repellat repudiandae rerum saepe,
+                similique?
+            </p>
+            <div class="article_footer">
+                <div class="tags">
+                    <img src="images/hashtag.png" alt="" width="15">
+                    <a href="">Fun</a>
+                    <a href="">Cinéma</a>
+                    <a href="">Espace</a>
+                </div>
+                <div class="footer_logos">
 
+                    <a href=""><img src="images/like.png" alt="like" width="15">&nbsp;40</a>
+
+                    <a href=""><img src="images/comments.png" alt="comments" width="15">&nbsp;20</a>
+
+                    <a href=""><img src="images/share" alt="share" width="15">&nbsp;Partager</a>
+                </div>
+            </div>
         </article>
     </section>
 
