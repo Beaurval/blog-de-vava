a81009c876041b17b855ea102d30bff661062c26
Valentin Beaury <beaury.valentin@gmail.com>
2019-01-10 09:12:55 +0100

    add files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Blog de Vava</title>
+</head>
+<body>
+<header class="header">
+    <nav class="nav_bar">
+        <img id="logo" src="images/logo.png" alt="logo">
+    </nav>
+</header>
+</body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Valentin Beaury
+ * Date: 10/01/2019
+ * Time: 09:02
+ */
